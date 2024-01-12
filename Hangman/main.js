71f6548eb3fd64266f40b3e9eec5c84518c6bf63
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-01-12 13:13:17 -0500

    sean

--- Hangman/main.js
+++ Hangman/main.js
@@ -1,39 +1,124 @@
- const WorderPull = "https://random-word-api.herokuapp.com/word"
-const DOMselectors = {
+/* 
+im getting rid of api
+stupid things wont let me make it globally
+jsut make a huge array of random words
+*/
 
-  word: document.getElementById("RANWord")
+
+const words = [
+  'statuesque',
+  'extend',
+  'solid',
+  'pine',
+  'gather',
+  'reminiscent',
+  'health',
+  'jog',
+  'quack',
+  'nosy',
+  'spade',
+  'oil',
+  'necessary',
+  'wary',
+  'count',
+  'blow',
+  'office',
+  'languid',
+  'unusual',
+  'earthy',
+  'messy',
+  'nice',
+  'cycle',
+  'interrupt',
+  'uninterested',
+  'avoid',
+  'aquatic',
+  'whimsical',
+  'war',
+  'rat',
+  'superb',
+  'sticks',
+  'existence',
+  'afford',
+  'roomy',
+  'legal',
+  'cats',
+  'homeless',
+  'ad',
+  'snow',
+  'resolute',
+  'fair',
+  'damage',
+  'drown',
+  'soda',
+  'secondhand',
+  'yam',
+  'ocean',
+  'mean',
+  'entertain',
+  'two',
+  'free',
+  'hypnotic',
+  'pricey',
+  'burst',
+  'ski',
+  'maniacal',
+  'erratic',
+  'suspend',
+  'invent',
+  'near',
+  'moor',
+  'driving',
+  'heap',
+  'aloof',
+  'injure',
+  'hill',
+  'tempt',
+  'blushing',
+  'chase',
+  'zesty',
+  'education',
+  'light',
+  'mother',
+  'labored',
+  'volcano',
+  'exciting',
+  'profit',
+  'powerful',
+  'cave',
+  'dashing',
+  'cure',
+  'able',
+  'imported',
+  'scary',
+  'erect',
+  'foamy',
+  'roof',
+  'border',
+  'legs',
+  'park',
+  'overrated',
+  'flimsy',
+  'fortunate',
+  'birthday',
+  'desert',
+  'bang',
+  'arch',
+  'understood',
+  'reign'
+]
+
+
+let random_num = Math.floor(Math.random() * 100)
+let selected_word = words[random_num]
+
+ if(letter_choice in selected_word){
+  console.log('YUH')
 } 
-
-let data2;
-
-async function getdata(url) { 
-  try {
-    const repsonce = await fetch(url); 
-    if (repsonce.status != 200) {
-      throw new Error(repsonce.statusText) 
-     } 
-    
-    data2 = await repsonce.json()
-    console.log(data2)
-
-    }
-
-catch (error){
-  error }
+/* while(1){
+  console.log('Ballsack\n')
+}
 
 
-
-} 
-
-getdata(WorderPull)
-
-
-
-console.log("wdwd")
-
-
-
-
-
-
-
+sean wrote this btw
+ */
