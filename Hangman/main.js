4ce4df7a89cad4067605a7a9244fc4614edb730a
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-01-18 13:04:53 -0500

    stuff

--- Hangman/main.js
+++ Hangman/main.js
@@ -110,17 +110,35 @@ let selected_word = words[random_num]
 console.log(selected_word)
 
 
-let letter_choice = 'a'; // make user iinput later
 
+
+
+
+function showletter() {  
+const attempts1 = []
+const attempts2 = []
+let guesses = 6
+while (guesses > 0 ){
+
+let letter_choice = prompt('Choose a letter')
+// add button so tha t you need to click to guess because its to annoying to pop up so often
+
+console.log(attempts)
 if (selected_word.includes(letter_choice)) {
   console.log('YUH');
+  attempts1.push(letter_choice)
+  // check to make sure that all letters in here = to word. check to make sure if u guess the word yet
 } else {
   console.log('Nope');
+  guesses -= 1
+  attempts2.push(letter_choice)
 }
-
-let displayed_word = selected_word
+  let displayed_word = selected_word
   .split('')
   .map(letter => (letter === letter_choice ? letter : '-'))
   .join('');
-
-console.log(displayed_word);
+  // need to make it so it keeps it cause right now if you guess correct its goes --a-- but if u guess wrong it resets to ----- and even correct again erases last attempt
+  console.log(displayed_word);
+  console.log(guesses)}
+}
+showletter()
